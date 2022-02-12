20d2786781b0a72e1fbf4663c0847cb8cbd19511
Pitermol <Pitersham@gmail.com>
2022-02-12 22:53:18 +0300

    added base.html and lokal bootstrap

--- backend/query.py
+++ backend/query.py
@@ -4,39 +4,42 @@ from collections import defaultdict
 import pandas as pd
 
 
-def get_stat(uid, per): #date format: YYYY-mm-dd
+def get_stat(uid, per):  # date format: YYYY-mm-dd
     dic = defaultdict(int)
     date = datetime.date(datetime.today() - timedelta(weeks=4 * per))
     for expense in Expenses.query.filter_by(user_id=uid).all():
         if datetime.date() >= date:
             dic[expense.cat] += expense.price
-    return list(dic.items()) #[(cat1, price1), (cat2, price2)]
+    return list(dic.items())  # [(cat1, price1), (cat2, price2)]
 
-def get_data(uid, cat): #date format: YYYY-mm-dd 
+
+def get_data(uid, cat):  # date format: YYYY-mm-dd
     dic = defaultdict(int)
     months = set()
     for expense in Expenses.query.filter_by(user_id=uid).all():
         months.add(expense.date[:7])
     for expense in Expenses.query.filter_by(user_id=uid).all():
         if expense.cat == cat:
-            dic[expense.date[:7]]+=expense.price
-    return list(dic.items()) #[(date1, price1), (date2, price2)] date format: YYYY-mm
+            dic[expense.date[:7]] += expense.price
+    return list(dic.items())  # [(date1, price1), (date2, price2)] date format: YYYY-mm
 
-def get_data_for_xlx(uid, sdate, edate): #date format: YYYY-mm-dd
-    sdate, edate = datetime.date(datetime.strptime(sdate, "%Y-%m-%d")), datetime.date(datetime.strptime(edate, "%Y-%m-%d"))
+
+def get_data_for_xlx(uid, sdate, edate):  # date format: YYYY-mm-dd
+    sdate, edate = datetime.date(datetime.strptime(sdate, "%Y-%m-%d")), datetime.date(
+        datetime.strptime(edate, "%Y-%m-%d"))
     dicex = []
     dicprof = []
     diccat = []
     dicexpr = []
     dicprofpr = []
     for expense in Expenses.query.filter_by(user_id=uid).all():
-        if sdate<=datetime.date(datetime.strptime(expense.date, "%Y-%m-%d"))<=edate:
+        if sdate <= datetime.date(datetime.strptime(expense.date, "%Y-%m-%d")) <= edate:
             date = f"{expense.date[8:]}.{expense.date[5:7]}.{expense.date[:4]}"
             dicex.append(date)
             diccat.append(expense.cat)
             dicexpr.append(expense.price)
     for profit in Profits.query.filter_by(user_id=uid).all():
-        if sdate<=datetime.date(datetime.strptime(profit.date, "%Y-%m-%d"))<=edate:
+        if sdate <= datetime.date(datetime.strptime(profit.date, "%Y-%m-%d")) <= edate:
             date = f"{profit.date[8:]}.{profit.date[5:7]}.{profit.date[:4]}"
             dicprof.append(date)
             dicprofpr.append(expense.price)
--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from backend.models import app
 from flask import jsonify, render_template, request
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/first_page.html", methods = ["GET", "POST"])
+
+@app.route("/first_page.html", methods=["GET", "POST"])
 def firsr_page():
     b = ""
     if request.method == "POST":
         login = request.form.get("login")
         if login == "123":
             b = 1
-    return render_template("first_page.html", bl = b)
+    return render_template("first_page.html", bl=b)
+
 
 @app.route("/history.html")
 def f():
     print(235)
     return render_template("history.html")
 
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
