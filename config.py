3fb2dfa79a55eecdb917005337f83d59aad51fe7
Pitermol <30591443+Pitermol@users.noreply.github.com>
2022-02-12 22:59:24 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from hashlib import md5
-
-SECRET_KEY = md5("PredProffessonal".encode('utf8')).hexdigest()
-DEBUG = True
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'site.db')
