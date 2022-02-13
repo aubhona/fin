a55a1331315167c06b676ffdf6e28c541f2b6806
Aub Abdullaev <aubhon.a@gmail.com>
2022-02-13 19:36:25 +0300

    Add api nds

--- service.py
+++ service.py
@@ -5,6 +5,8 @@ from backend.query import *
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import pandas as pd
+import requests
+import json
 
 matplotlib.use('Agg')
 
@@ -221,3 +223,23 @@ def oper_add(uid, price, date, cat = None):
 
 def get_expences(uid, per):
     return get_db_expences(uid, per)
+
+def recalculate_balance(uid, period) -> float:
+    # PV = get_balance(uid=uid)
+    PV = 100
+
+    url = "https://apidata.mos.ru/v1/datasets/62025/rows?api_key=d6dd03633051cdca213e0a016186697f&$orderby=global_id"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    text = response.text
+    data = json.loads(text)[-1]
+    IPC = float(data["Cells"]["Value"])
+
+    FV = PV * (1. - (period / 12.) * ((IPC - 100.) / 100.))
+    if FV < 0:
+        FV = 0.
+
+    return FV
